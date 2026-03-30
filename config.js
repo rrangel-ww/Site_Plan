a6a4973f9a8d4e91e4a93c3d3c63d9ab4522735f
rrangel-ww <rrangel@wwcowa.gov>
2026-03-30 10:58:44 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,70 @@
+var SitePlanConfig = {
+  map: {
+    center: [-118.26, 46.14],
+    extent: { xmin: -118.64, ymin: 45.82, xmax: -117.88, ymax: 46.47 },
+    basemap: "hybrid"
+  },
+
+  layers: {
+    parcels: {
+      url: "https://services8.arcgis.com/C0L6rRPkF9w28VGX/arcgis/rest/services/Parcel_Information_Report/FeatureServer/0",
+      outFields: ["*"],
+
+      popupFields: {
+        parcelNumber:   "geo_id",
+        ownerName:      "py_owner_n",
+        siteAddress:    "full_situs",
+        acreage:        "size_acres",
+        legalDesc:      "legal_desc",
+        fireDistrict:   "FIRE_DIST",
+        platBookPage:   "PLAT_BK_PG",
+        platLink:       "PLAT_LINK",
+        assessorLink:   "link_inter",
+
+        mailAddr1:      "py_addr__1",
+        mailCity:       "py_addr_ci",
+        mailState:      "py_addr_st",
+        mailZip:        "py_addr_zi",
+
+        zoningName:     "ZONE_NAME",
+        zoningAbbrev:   "ZONE_ABBREV",
+        landUseName:    "LU_NAME",
+        landUseAbbrev:  "LU_ABBREV",
+        uga:            "UGA_NAME",
+        shorelineDes:   "SHR_DES",
+        shorelineAbbr:  "SHR_ABBREV",
+        millCreek:      "MILL_CREEK_WPP",
+        setbackFront:   "SETBACK_FRONT",
+        setbackSide:    "SETBACK_SIDE",
+        setbackRear:    "SETBACK_REAR",
+        setbackNote:    "SETBACK_NOTE",
+
+        caErosion:      "CA_EROSION_HAZ",
+        caRipName:      "CA_RIP_NAME",
+        caRipBuffer:    "CA_RIP_BUFFER",
+        caFloodRisk:    "CA_FLOOD_RISK",
+        caFloodZone:    "CA_FLOOD_ZONE",
+        caCaraHigh:     "CA_CARA_HIGH",
+        caCaraMod:      "CA_CARA_MOD",
+        caWetland:      "CA_WETLAND",
+        caLiquefaction: "CA_LIQUEFACTION",
+        caHawkHab:      "CA_HAB_FER_HAWK",
+        caSongbird:     "CA_HAB_NEO_SONG",
+        caWinterBirds:  "CA_HAB_WIN_PREY"
+      }
+    }
+  },
+
+  branding: {
+    countyName:    "Walla Walla County",
+    agencyName:    "Community Development",
+    toolTitle:     "Site Plan Builder",
+    printHeader:   "Walla Walla County — Permit Application Site Plan",
+    sealUrl:       "https://jobs.wsac.org/wp-content/uploads/job-manager-uploads/company_logo/2022/03/Walla-Walla-County-Seal-147x150.jpg",
+    parcelInfoUrl: "https://experience.arcgis.com/experience/7133ad29736547f2b24bbd0f1753bc24",
+    phone:         "509-524-2610",
+    address:       "310 W. Poplar Street, Suite 200 · Walla Walla, WA 99362",
+    formsUrl:      "https://www.wwcowa.gov/government/community_development/applications.php",
+    municodeUrl:   "https://library.municode.com/wa/walla_walla_county/codes/code_of_ordinances?nodeId=TIT18EN_CH18.08CRARPR"
+  }
+};
